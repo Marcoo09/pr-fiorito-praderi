49f803569739864a0f5ccb627326577fa479e0bd
Marco Fiorito <marcofiorito1@gmail.com>
2021-11-22 16:09:05 -0300

    add empty projects

---
+++ Obligatorio/Logger/Logger.BusinessLogic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Logger.BusinessLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
