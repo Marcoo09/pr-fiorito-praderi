da72793828830d7151a1821d1619fa672185d4a8
Marco Fiorito <marcofiorito1@gmail.com>
2021-11-22 00:27:09 -0300

    add IndexUsers on controller

--- Obligatorio/AdminServer/AdminServer/Controllers/UserController.cs
+++ Obligatorio/AdminServer/AdminServer/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AdminServer.Models.Request;
 using AdminServer.Models.Response;
+using Google.Protobuf.WellKnownTypes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AdminServer.Controllers
@@ -65,91 +66,24 @@ namespace AdminServer.Controllers
             }
         }
 
-        //[HttpPut("{id:int}")]
-        //public async Task<IActionResult> UpdatePost(int id, [FromBody] UpdatePostRequestModel updatePostModel)
-        //{
-        //    UpdatePostRequest request = new UpdatePostRequest()
-        //    {
-        //        Id = id,
-        //        Name = updatePostModel.Name
-        //    };
-        //    UpdatePostResponse response = await _postClient.UpdatePostAsync(request);
-        //    if (response.Ok)
-        //    {
-        //        PostBasicInfo postBasicInfo = response.Post;
-        //        return Ok(postBasicInfo);
-        //    }
-        //    else
-        //    {
-        //        Error error = response.Error;
-        //        return BadRequest(error);
-        //    }
-        //}
+        [HttpGet]
+        public async Task<IActionResult> IndexUsers(int id)
+        {
+            IndexUsersResponse response = await _userClient.IndexUsersAsync(new Empty());
+            if (response.Ok)
+            {
+                List<UserDetail> users = response.Users.ToList();
 
-        //[HttpPut("{id:int}/theme")]
-        //public async Task<IActionResult> UpdatePost(int id, [FromBody] ChangePostThemeRequestModel changePostThemeRequestModel)
-        //{
-        //    ChangePostThemeRequest request = new ChangePostThemeRequest()
-        //    {
-        //        PostId = id,
-        //        ThemeId = changePostThemeRequestModel.ThemeId
-        //    };
-        //    ChangePostThemeResponse response = await _postClient.ChangePostThemeAsync(request);
-        //    if (response.Ok)
-        //    {
-        //        PostDetailInfo postDetailInfo = response.Post;
-        //        return Ok(postDetailInfo);
-        //    }
-        //    else
-        //    {
-        //        Error error = response.Error;
-        //        return BadRequest(error);
-        //    }
-        //}
-
-        //[HttpDelete("{id:int}")]
-        //public async Task<IActionResult> DeletePost(int id)
-        //{
-        //    DeletePostRequest request = new DeletePostRequest()
-        //    {
-        //        Id = id,
-        //    };
-        //    DeletePostResponse response = await _postClient.DeletePostAsync(request);
-        //    if (response.Ok)
-        //    {
-        //        Message message = response.Message;
-        //        return Ok(message);
-        //    }
-        //    else
-        //    {
-        //        Error error = response.Error;
-        //        return BadRequest(error);
-        //    }
-        //}
-
-        //[HttpGet]
-        //public async Task<IActionResult> IndexPostsByTheme([FromQuery] PaginationRequestModel paginationRequestModel,
-        //    [FromQuery] IndexPostsByThemeRequest indexPostsByThemeRequest)
-        //{
-        //    IndexPostsByThemeResponse response = await _postClient.IndexPostsByThemeAsync(indexPostsByThemeRequest);
-        //    if (response.Ok)
-        //    {
-        //        List<PostDetailInfo> posts = response.Posts.ToList();
-        //        int offset = paginationRequestModel.CalculateOffset();
-
-        //        return Ok(new IndexPostsByThemeResponseModel()
-        //        {
-        //            Page = paginationRequestModel.Page,
-        //            PageSize = paginationRequestModel.PageSize,
-        //            Total = posts.Count,
-        //            Posts = posts.Skip(offset).Take(paginationRequestModel.PageSize).ToList()
-        //        });
-        //    }
-        //    else
-        //    {
-        //        Error error = response.Error;
-        //        return BadRequest(error);
-        //    }
-        //}
+                return Ok(new IndexUsersResponseModel()
+                {
+                    Users = users
+                });
+            }
+            else
+            {
+                Error error = response.Error;
+                return BadRequest(error);
+            }
+        }
     }
 }
