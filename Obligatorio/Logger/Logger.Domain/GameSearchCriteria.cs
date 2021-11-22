b272ddf5f4b20bcbe1eb294a36321f7f106d7f68
Marco Fiorito <marcofiorito1@gmail.com>
2021-11-22 17:34:25 -0300

    add search criteria

---
+++ Obligatorio/Logger/Logger.Domain/GameSearchCriteria.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Logger.Domain;
+using Logger.Domain.Interfaces;
+using DTOs.Response;
+
+namespace LogsServer.Domain.SearchCriteria
+{
+    public class GameSearchCriteria : ISearchCriteria<Log>
+    {
+        public int? Id { get; set; }
+        public string Title { get; set; }
+
+        public bool MatchesCriteria(Log logGame)
+        {
+            bool matchesCriteria = false;
+
+            if (logGame.EntityType == typeof(GameDetailDTO) || logGame.EntityType == typeof(List<GameBasicInfoDTO>))
+            {
+                matchesCriteria = MatchesId(logGame) && MatchesTitle(logGame) &&
+                       MatchesLogTag(logGame);
+            }
+
+            return matchesCriteria;
+        }
+
+        private bool MatchesId(Log logGame)
+        {
+            bool matchesId = true;
+
+            if (Id != null)
+            {
+                if (logGame.IsEntityAList())
+                {
+                    List<GameBasicInfoDTO> themes = logGame.Entity as List<GameBasicInfoDTO>;
+                    matchesId = themes.Any(t => t.Id == Id);
+                }
+                else
+                {
+                    GameBasicInfoDTO theme = logGame.Entity as GameBasicInfoDTO;
+                    matchesId = theme.Id == Id;
+                }
+            }
+
+            return matchesId;
+        }
+
+        private bool MatchesTitle(Log logGame)
+        {
+            bool matchesTitle = true;
+
+            if (!String.IsNullOrEmpty(Title))
+            {
+                if (logGame.IsEntityAList())
+                {
+                    List<GameBasicInfoDTO> games = logGame.Entity as List<GameBasicInfoDTO>;
+                    matchesTitle = games.Any(t => t.Title.ToLower().Contains(Title.ToLower()));
+                }
+                else
+                {
+                    GameBasicInfoDTO game = logGame.Entity as GameBasicInfoDTO;
+                    matchesTitle = game.Title.ToLower().Contains(Title.ToLower());
+                }
+            }
+
+            return matchesTitle;
+        }
+
+        private bool MatchesLogTag(Log logGame)
+        {
+            return logGame.Tag == Tag.CreateGame || logGame.Tag == Tag.IndexBoughtGames ||
+                   logGame.Tag == Tag.GetGame || logGame.Tag == Tag.IndexGamesCatalog ||
+                   logGame.Tag == Tag.SearchGames || logGame.Tag == Tag.BuyGame ||
+                   logGame.Tag == Tag.CreateGameReview || logGame.Tag == Tag.DeleteGame;
+        }
+    }
+}
---
+++ Obligatorio/Logger/Logger.Domain/UserSearchCriteria.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Logger.Domain;
+using Logger.Domain.Interfaces;
+using DTOs.Response;
+
+namespace LogsServer.Domain.SearchCriteria
+{
+    public class UserSearchCriteria : ISearchCriteria<Log>
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+
+        public bool MatchesCriteria(Log logUser)
+        {
+            bool matchesCriteria = false;
+
+            if (logUser.EntityType == typeof(UserDetailDTO))
+            {
+                matchesCriteria = MatchesId(logUser) && MatchesName(logUser) &&
+                       MatchesLogTag(logUser);
+            }
+
+            return matchesCriteria;
+        }
+
+        private bool MatchesId(Log logUser)
+        {
+            bool matchesId = true;
+
+            if (Id != null)
+            {
+                if (logUser.IsEntityAList())
+                {
+                    List<UserDetailDTO> users = logUser.Entity as List<UserDetailDTO>;
+                    matchesId = users.Any(t => t.Id == Id);
+                }
+                else
+                {
+                    UserDetailDTO user = logUser.Entity as UserDetailDTO;
+                    matchesId = user.Id == Id;
+                }
+            }
+
+            return matchesId;
+        }
+
+        private bool MatchesName(Log logUser)
+        {
+            bool matchesName = true;
+
+            if (!String.IsNullOrEmpty(Name))
+            {
+                if (logUser.IsEntityAList())
+                {
+                    List<UserDetailDTO> users = logUser.Entity as List<UserDetailDTO>;
+                    matchesName = users.Any(u => u.Name.ToLower().Contains(Name.ToLower()));
+                }
+                else
+                {
+                    UserDetailDTO user = logUser.Entity as UserDetailDTO;
+                    matchesName = user.Name.ToLower().Contains(Name.ToLower());
+                }
+            }
+
+            return matchesName;
+        }
+
+        private bool MatchesLogTag(Log logGame)
+        {
+            return logGame.Tag == Tag.Login || logGame.Tag == Tag.IndexUsers;
+        }
+    }
+}
